a74f831b98518d537f3ec8b851a7e2622c050b24
Suwat Inkaew <swik999@gmail.com>
2021-01-08 16:14:03 +0700

    Change

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -6,12 +6,14 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	string textline;
+	cout << "-------------------- BOOM ---------------------" << "\n";
+	while (getline(source,textline)){
+		cout << textline << "\n";
+	}
+	cout << "-------------------- HA!! ---------------------" << "\n";
     source.close();
     dest.close();
 	return 0;
